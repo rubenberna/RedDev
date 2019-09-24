cf6f1cd6c540a11877d0963d8f04e518fb1fb3b9
rubenberna <rubenmbernardes@gmail.com>
2019-09-24 12:12:38 +0200

    style(): different positioning of the assign buttons

--- client/src/views/TaskView.js
+++ client/src/views/TaskView.js
@@ -48,20 +48,29 @@ class TaskView extends Component {
   render() {
     const { task } = this.props.location.state
     return(
-      <div className='task-view container'>
-        <h4>{task.title}</h4>
-        <hr/>
-        <div className='task-details'>
-          <h6><span className='task-spec'>Requester: </span>{task.requester}</h6>
-          <h6><span className='task-spec'>When? </span>{moment(task.reqDate).format("MMM Do")}</h6>
-          <h6><span className='task-spec'>Description: </span>{task.description}</h6>
-          <h6><span className='task-spec'>Type: </span>{task.type}</h6>
-          <h6><span className='task-spec'>Assigned to: </span><span style={{ cursor: 'pointer' }} onClick={ e => this.viewDevProfile(task.dev) }>{task.dev ? task.dev : ''}</span></h6>
-          <h6><span className='task-spec'>Status: </span><span className={task.status === 'ongoing' ? 'ongoing' : 'complete'}>{task.status}</span></h6>
+      <>
+        <div className='task-view container'>
+          <h4>{task.title}</h4>
+          <hr/>
+          <div className='task-details'>
+            <h6><span className='task-spec'>Requester: </span>
+              <a
+              href={`mailto:${task.requester}?Subject=${task.title}`}
+              target="_blank"
+              rel="noopener noreferrer">
+              { task.requester }
+              </a>
+            </h6>
+            <h6><span className='task-spec'>When? </span>{moment(task.reqDate).format("MMM Do")}</h6>
+            <h6><span className='task-spec'>Description: </span>{task.description}</h6>
+            <h6><span className='task-spec'>Type: </span>{task.type}</h6>
+            <h6><span className='task-spec'>Assigned to: </span><span style={{ cursor: 'pointer' }} onClick={ e => this.viewDevProfile(task.dev) }>{task.dev ? task.dev : ''}</span></h6>
+            <h6><span className='task-spec'>Status: </span><span className={task.status === 'ongoing' ? 'ongoing' : 'complete'}>{task.status}</span></h6>
+          </div>
+          { this.renderFinishButton() }
         </div>
         { this.renderAssignSelection() }
-        { this.renderFinishButton() }
-      </div>
+      </>
     )
   }
 }
